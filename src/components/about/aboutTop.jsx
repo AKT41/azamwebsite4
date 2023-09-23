f5aaf0fd344d12572f494bd7f3234d265520d8b0
Ahmet Kemal Türker <ahmet.turker@ogr.gelisim.edu.tr>
2023-09-23 14:21:44 +0300

    aboutUs add

--- src/components/about/aboutTop.jsx
+++ src/components/about/aboutTop.jsx
@@ -3,9 +3,18 @@ import { useTranslation } from "react-i18next";
 import { Fade } from "react-reveal";
 
 import "./style/style.css";
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 
 function AboutTop() {
   const { t } = useTranslation();
+
+  const [showText, setShowText] = useState(false);
+
+  const toggleReadMore = () => {
+    setShowText(!showText);
+  };
+
   return (
     <>
       <div className="md:min-h-[screen]">
@@ -32,8 +41,8 @@ function AboutTop() {
             </p>
           </Fade>
         </div>
-        <div className="max-w-screen-xl mx-auto h-screen flex items-center flex-col justify-around md:-mt-24 -mt-16">
-          <div className="flex flex-col gap-8 md:flex-row justify-between items-center">
+        <div className="max-w-screen-xl mx-auto min-h-screen flex items-center flex-col justify-around md:-mt-24 -mt-16">
+          <div className="flex flex-col gap-8 md:flex-row justify-between items-center h-full">
             <div className="md:w-1/2 w-8/12">
               <Tilt
                 tiltMaxAngleX={5}
@@ -55,18 +64,43 @@ function AboutTop() {
               </Tilt>
             </div>
 
-            <div className="w-full">
+            <div className="w-full h-full mb-10 md:my-40">
               <h1
                 className="text-4xl text-gray-900 px-4 underline-style"
                 style={{ fontFamily: "Gilroy-Medium" }}
               >
-                <Fade top> {t("AboutUsTitle")}</Fade>
+                {t("AboutUsTitle")}
               </h1>
               <p
-                className="text-xl text-gray-800 mt-7 p-4 pr-6"
+                className="text-xl text-gray-800 mt-7 p-4 pr-6 pb-0"
                 style={{ fontFamily: "Gilroy-Light" }}
               >
-                <Fade right> {t("AboutUsText")}</Fade>
+                {t("AboutUsText")}
+              </p>
+              <CSSTransition
+                in={showText}
+                timeout={300}
+                classNames="fade"
+                unmountOnExit
+              >
+                <div style={{ fontFamily: "Gilroy-Light" }}>
+                  <p className="text-xl text-gray-800 mt-7 p-4 pr-6 pb-0">
+                    {t("AboutUsText2")}
+                  </p>
+                  <p className="text-xl text-gray-800 mt-7 p-4 pr-6 pb-0">
+                    {t("AboutUsText3")}
+                  </p>
+                  <p className="text-xl text-gray-800 mt-7 p-4 pr-6 pb-0">
+                    {t("AboutUsText4")}
+                  </p>
+                </div>
+              </CSSTransition>
+              <p
+                className="text-blue-500 cursor-pointer pl-4 pt-2"
+                style={{ fontFamily: "Gilroy-Light" }}
+                onClick={toggleReadMore}
+              >
+                {showText ? t("ShowLess") : t("ReadMore")}
               </p>
             </div>
           </div>
